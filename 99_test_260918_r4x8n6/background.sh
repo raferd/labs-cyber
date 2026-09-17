baa3e666986351a545c8ede43a32fed3874e8a4c
raferd <47186325+raferd@users.noreply.github.com>
2026-09-17 23:49:07 +0200

    Change process ID file name in background.sh

--- 99_test_260918_r4x8n6/background.sh
+++ 99_test_260918_r4x8n6/background.sh
@@ -47,4 +47,4 @@ chmod -R a+rX "$BASE"
 chmod 640 "$BASE/documentos/contrato.txt"
 
 nohup sleep 3600 >/dev/null 2>&1 &
-echo $! > "$BASE/sistema/proceso.pid"
+echo $! > "$BASE/sistema/proceso_old.pid"
